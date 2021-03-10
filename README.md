5c49247b6657458ff18b6db14de28b287e3a45a4
rodeok <nationx22@gmail.com>
2021-03-10 18:30:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fast-design-27egek
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/fast-design-27egek)
---
+++ index.ts
@@ -0,0 +1,24 @@
+import {
+  FluentDesignSystemProvider,
+  FluentButton,
+  FluentCheckbox,
+  FluentTextField,
+  FluentCard,
+  FluentSlider,
+  FluentSliderLabel
+} from "@fluentui/web-components";
+import { TodoApp } from "./src/todo-app";
+import { TodoForm } from "./src/todo-form";
+import { DesignPropertyPanel } from "./src/design-property-panel";
+
+FluentDesignSystemProvider;
+FluentButton;
+FluentCheckbox;
+FluentTextField;
+FluentCard;
+FluentSlider;
+FluentSliderLabel;
+
+TodoForm;
+TodoApp;
+DesignPropertyPanel;
